f3112b957f2233a950c71652acf8430a9d8896c0
Nick Lombardo <nicholasl@sccwrp.org>
2024-06-19 08:57:55 -0700

    added option for specifiying multiple thresholds

--- R/responseModelsServer.R
+++ R/responseModelsServer.R
@@ -1,31 +1,68 @@
 responseModelsServer <- function(id) {
   moduleServer(id, function(input, output, session) {
+    # threshold user input
+    thresh_goal_table <- reactiveValues(data = {
+      data.frame(
+        Index = c("CSCI", "ASCI_D", "ASCI_H"),
+        Goal = c(0.63, 0.75, 0.75)
+      )
+    })
+    
+    output$user_input_table <- DT::renderDataTable({
+      thresh_goal_table$data
+    }, 
+    editable = list(target = "cell", disable = list(columns = c(0))), 
+    options = list(dom = 't', ordering = FALSE), 
+    selection = 'none',
+    rownames = FALSE
+    ) |>
+      bindEvent(thresh_goal_table$data)
+    
+    
+    observe({
+      thresh_goal_table$data <<- DT::editData(thresh_goal_table$data, input$user_input_table_cell_edit, rownames = FALSE)
+    }) |>
+      bindEvent(input$user_input_table_cell_edit)
+    
+    selected_indices <- reactive({
+      thresh_goal_table$data$Index[!is.na(thresh_goal_table$data$Goal)]
+    })
+    
     #function to calculate goal
     thresh_dat <- reactive({
       raw_dat |>
         dplyr::rename(`Eutrophication Variable` = BiostimVar) |>
         dplyr::filter(Stressor %in% input$biostim) |> 
-        dplyr::filter(Index %in% input$index) |> 
-        dplyr::filter(Fit>=input$goal) |> 
+        dplyr::filter(Index %in% selected_indices()) |> 
+        dplyr::inner_join(thresh_goal_table$data, by = dplyr::join_by(Index, Fit >= Goal)) |> 
         dplyr::group_by(Stressor,Index) |>
         dplyr::slice_max(BiostimValue, n=1) |>
         dplyr::ungroup() |>
         dplyr::rename(`Threshold Candidate`=BiostimValue, 
-               IndexScore_predicted=Fit,
-               IndexScore_predicted_se=SE) |>
+               IndexScore_predicted = Fit,
+               IndexScore_predicted_se = SE) |>
         dplyr::mutate(
-          IndexScore_predicted_l95 = IndexScore_predicted - IndexScore_predicted_se * 1.96,
-          IndexScore_predicted_u95 = IndexScore_predicted + IndexScore_predicted_se * 1.96,
+          IndexScore_predicted_l95 = round(IndexScore_predicted - IndexScore_predicted_se * 1.96, 2),
+          IndexScore_predicted_u95 = round(IndexScore_predicted + IndexScore_predicted_se * 1.96, 2),
+          IndexScore_predicted_se = round(IndexScore_predicted_se, 3),
+          IndexScore_predicted = round(IndexScore_predicted, 2),
           `Threshold Candidate` = dplyr::case_when(
             Stressor == 'Total N' ~ round(`Threshold Candidate`, 3),
             Stressor == 'Total P' ~ round(`Threshold Candidate`, 3),
             Stressor == 'Chl-a' ~ round(`Threshold Candidate`, 1),
             Stressor == 'AFDM' ~ round(`Threshold Candidate`, 1),
             Stressor == '% cover' ~ round(`Threshold Candidate`)
+          ),
+          Units = dplyr::case_when(
+            Stressor == 'Total N' ~ 'mg/L',
+            Stressor == 'Total P' ~ 'mg/L',
+            Stressor == 'Chl-a' ~ 'mg/m2',
+            Stressor == 'AFDM' ~ 'g/m2',
+            Stressor == '% cover' ~ '%'
           )
         ) |>
         dplyr::select(
-          `Eutrophication Variable`, Stressor, `Threshold Candidate`, Index, 
+          Stressor, Units, `Threshold Candidate`, Index, 
           IndexScore_predicted, IndexScore_predicted_se, IndexScore_predicted_l95, IndexScore_predicted_u95
         )
     })
@@ -33,13 +70,16 @@ responseModelsServer <- function(id) {
     #function to render table using above function 
     output$table <- DT::renderDataTable({
       thresh_dat()
-    }) |>
+    }, 
+      options = list(pageLength = 6, searching = FALSE, bLengthChange = FALSE), 
+      selection = 'none'
+    ) |>
       bindEvent(input$submit)
     
     # #function to create plots
     plot_data <- reactive({
       plot_raw_dat <- raw_dat |>
-        dplyr::filter(Stressor %in% input$biostim, Index %in% input$index) |>
+        dplyr::filter(Stressor %in% input$biostim, Index %in% selected_indices()) |>
         dplyr::mutate(
           Stressor = case_when(
             Stressor == 'Total N' ~ 'Total N (mg/L)',
@@ -55,7 +95,7 @@ responseModelsServer <- function(id) {
         )
       
       plot_obs_df <- obs_points_df |>
-        dplyr::filter(Stressor %in% input$biostim, Index %in% input$index) |>
+        dplyr::filter(Stressor %in% input$biostim, Index %in% selected_indices()) |>
         dplyr::mutate(
           Stressor = case_when(
             Stressor == 'Total N' ~ 'Total N (mg/L)',
@@ -84,7 +124,10 @@ responseModelsServer <- function(id) {
             levels = c('Total N (mg/L)', 'Total P (mg/L)', 'Chl-a (mg/m2)', 'AFDM (g/m2)', '% cover')
           )
         )
-      
+
+      if (all(nrow(plot_raw_dat) == 0, nrow(plot_obs_df) == 0, nrow(plot_thresh_dat) == 0)) {
+        return(NULL)
+      }
       ggplot(data = plot_raw_dat, aes(x = BiostimValue, y = Fit))+
         geom_point(data = plot_obs_df, aes(y = IndexScore), size = 0.5, color = 'gray') +
         geom_ribbon(
--- R/responseModelsUI.R
+++ R/responseModelsUI.R
@@ -4,21 +4,13 @@ responseModelsUI <- function(id) {
       '
         <h3 style="color:#2f5496">Eutrophication Response Models</h3>
         <p> Response models can help managers identify stress levels above which poor biological conditions are likely to occur. We calibrated a type of general additive model (shape-constrained additive models with monotonic negative relationships) between biointegrity index scores and eutrophication indicator concentrations. Managers can use these models to identify candidate eutrophication thresholds for a wide range of biointegrity goals for the CSCI, ASCI_D, or ASCI_H. </p>
-        <p>After entering the biointegrity goal and selecting the desired biointegrity and eutrophication indicators, the dashboard will provide a table of the resulting eutrophication thresholds, along with a plot showing the model response.</p>
+        <p>Enter or change threshold goal values by double-clicking each cell in the Goal column. Click the Submit button below the table to submit. After entering the biointegrity goals and selecting the desired biointegrity and eutrophication indicators, the dashboard will provide a table of the resulting eutrophication thresholds, along with plots showing the model responses. Plots and table entries will not be generated for indices with blank values in the Goal column.</p>
       '
     ),
     fluidRow(
       column(2,
-             numericInput(NS(id, "goal"), "Threshold goal", 
-                          value = 0.75, 
-                          min = 0, 
-                          max = 300, step = 0.05),
-             shinyWidgets::pickerInput(inputId = NS(id, "index"),
-                         label = "Index",
-                         choices = unique(raw_dat$Index),
-                         selected = c("CSCI", "ASCI_H", "ASCI_D"),
-                         options = list(`actions-box` = TRUE),
-                         multiple = TRUE),
+             DT::dataTableOutput(NS(id, "user_input_table")),
+             br(),
              shinyWidgets::pickerInput(inputId = NS(id, "biostim"),
                          label = "Eutrophication Analyte",
                          choices = unique(raw_dat$Stressor),
