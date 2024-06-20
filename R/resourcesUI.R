e2932298e7a10b4a1f0d73763cd62d5a8dfbd5d0
Nick Lombardo <nicholasl@sccwrp.org>
2024-06-20 10:22:21 -0700

    converted Resources tab to bslib ui

--- R/resourcesUI.R
+++ R/resourcesUI.R
@@ -1,15 +1,9 @@
 resourcesUI <- function(id) {
-  page_fluid(
-    theme = bs_theme(preset = "cosmo"),
-    column(2),
-    column(
-      8,
+  tagList(
       h3("Resources"),
       p("This dashboard is based on findings from SCCWRP Technical Report #1367, the data and code for which is hosted on ", a(href = "https://github.com/SCCWRP/BiointegrityEutrophicationIndicators", target = "_blank", .noWS = "outside", "GitHub"), "."),
       p("The code for this dashboard is also hosted on ", a(href = "https://github.com/SCCWRP/ModifiedChannelThresholds", target = "_blank", .noWS = "outside", "GitHub"), "."),
       p("To download the full thresholds table used in this application, click ", downloadLink(NS(id, "thresh_table"), label = "here", .noWS = "outside"), "."),
       p("To download a spreadsheet containing data sets used in each analysis, click ", downloadLink(NS(id, "data_sets"), label = "here", .noWS = "outside"), ". A data dictionary is included.")
-    ),
-    column(2)
-  )
+    )
 }
