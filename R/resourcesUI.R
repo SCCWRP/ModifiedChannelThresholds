36556ba27d493e2038f124c7815571fe57f46c95
nmlombardo <nicholaslombardo97@gmail.com>
2024-06-23 19:58:32 -0700

    changed resources tab to fixed page layout

--- R/resourcesUI.R
+++ R/resourcesUI.R
@@ -1,5 +1,5 @@
 resourcesUI <- function(id) {
-  tagList(
+  page_fixed(
       h3("Resources"),
       p("This dashboard is based on findings from SCCWRP Technical Report #1367, the data and code for which is hosted on ", a(href = "https://github.com/SCCWRP/BiointegrityEutrophicationIndicators", target = "_blank", .noWS = "outside", "GitHub"), "."),
       p("The code for this dashboard is also hosted on ", a(href = "https://github.com/SCCWRP/ModifiedChannelThresholds", target = "_blank", .noWS = "outside", "GitHub"), "."),
