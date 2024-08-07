4bf3149c18d406b7a117ccc0f39e4be1ba7f55aa
Nick Lombardo <nicholasl@sccwrp.org>
2024-08-07 11:37:25 -0700

    updated thresholds with last sample exclusion as of 7/15

--- data-raw/thresholds.R
+++ data-raw/thresholds.R
@@ -1,3 +1,3 @@
-thresholds <- readxl::read_excel("data-raw/thresholds_07032024.xlsx")
+thresholds <- readxl::read_excel("data-raw/thresholds_08072024.xlsx")
 
 usethis::use_data(thresholds, overwrite = TRUE)
